643dc0083e8374002e232b47a98d11321576fd6f
Jose Aleixo Araujo Porpino Filho <aleixoporpino@gmail.com>
2023-07-16 15:24:00 -0400

    Initial commit

---
+++ src/main/java/com/porpapps/wealthwatchapi/WealthWatchApiApplication.java
@@ -0,0 +1,16 @@
+package com.porpapps.wealthwatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WealthWatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthWatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porpapps/wealthwatchapi/WealthWatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porpapps.wealthwatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthWatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
