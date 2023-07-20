8a640689894b74bcd9743a69e2deba5ea029756e
Lucianna Mendonca <luciannamend@gmail.com>
2023-07-20 13:00:42 -0400

    update sql file

--- wealth-watch-script.sql
+++ wealth-watch-script.sql
@@ -1,26 +1,31 @@
 create table user
 (
-id int unsigned not null unique auto_increment,
-name varchar(45) not null unique,
+id int unsigned not null primary key auto_increment,
+name varchar(60) not null unique,
 password varchar(20) not null,
-primary key (id)
+email varchar(200) not null unique,
+created datetime not null,
+updated datetime not null,
 );
 
 create table portfolio
 (
-id int unsigned not null unique auto_increment,
+id int unsigned not null primary key auto_increment,
 user_id int unsigned not null,
 name varchar(50) not null unique,
 currency enum('CAD', 'USD', 'BRL') not null,
-primary key (id),
-constraint fk_portfolio_user foreign key (user_id) references user (id)
+created datetime not null,
+updated datetime not null,
+constraint fk_portfolio_user_id foreign key (user_id) references user (id)
 );
 
 create table portfolio_item
 (
-id int unsigned not null unique auto_increment,
+id int unsigned not null primary key auto_increment,
 portfolio_id int unsigned not null,
-symbol varchar(20) not null unique,
+created datetime not null,
+updated datetime not null,
+symbol varchar(20) not null,
 shares int,
 trade_date date,
 cost_share decimal(4,2),
@@ -28,8 +33,5 @@ day_gain decimal(3,2),
 total_gain decimal(5,2),
 high_limit decimal(4,2),
 low_limit decimal(4,2),
-primary key (id),
-constraint fk_portfolio_item_portfolio foreign key (portfolio_id) references portfolio (id)
+constraint fk_portfolio_item_portfolio_id foreign key (portfolio_id) references portfolio (id)
 );
-
-select * from portfolio_item;
