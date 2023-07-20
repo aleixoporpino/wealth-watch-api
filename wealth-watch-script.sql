982b7182ec7e3faa4bb2b52a182b67dc75315445
Lucianna Mendonca <121981547+luciannamend@users.noreply.github.com>
2023-07-20 17:43:31 -0400

    Create sql file and update dml (#1)
    * create sql file and update dml
    
    * update sql file
    
    * Create schema and Add uppercase keyword

---
+++ wealth-watch-script.sql
@@ -0,0 +1,40 @@
+CREATE SCHEMA IF NOT EXISTS wealth_watch;
+USE wealth_watch;
+
+CREATE IF NOT EXISTS TABLE user
+(
+id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(60) NOT NULL UNIQUE,
+password VARCHAR(20) NOT NULL,
+email VARCHAR(200) NOT NULL UNIQUE,
+created DATETIME NOT NULL,
+updated DATETIME NOT NULL,
+);
+
+CREATE IF NOT EXISTS TABLE portfolio
+(
+id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+user_id INT UNSIGNED NOT NULL,
+name VARCHAR(50) NOT NULL UNIQUE,
+currency ENUM('CAD', 'USD', 'BRL') NOT NULL,
+created DATETIME NOT NULL,
+updated DATETIME NOT NULL,
+CONSTRAINT fk_portfolio_user_id FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE IF NOT EXISTS TABLE portfolio_item
+(
+id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+portfolio_id INT UNSIGNED NOT NULL,
+symbol VARCHAR(20) NOT NULL,
+shares INT,
+trade_date DATE,
+cost_share DECIMAL(4,2),
+day_gain DECIMAL(3,2),
+total_gain DECIMAL(5,2),
+high_limit DECIMAL(4,2),
+low_limit DECIMAL(4,2),
+created DATETIME NOT NULL,
+updated DATETIME NOT NULL,
+CONSTRAINT fk_portfolio_item_portfolio_id FOREIGN KEY (portfolio_id) REFERENCES portfolio (id)
+);
