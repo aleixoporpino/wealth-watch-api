6338c00e3d2e34df19c51d237970c1c68303d465
Lucianna Mendonca <luciannamend@gmail.com>
2023-08-22 18:02:03 -0400

    GH-9 - Corrected some errors

--- src/main/java/com/porpapps/wealthwatchapi/repository/PortfolioItemRepository.java
+++ src/main/java/com/porpapps/wealthwatchapi/repository/PortfolioItemRepository.java
@@ -1,8 +1,10 @@
 package com.porpapps.wealthwatchapi.repository;
 
+import com.porpapps.wealthwatchapi.model.PortfolioItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PortfolioItemRepository extends JpaRepository<PortfolioItemRepository, Integer> {
+
+public interface PortfolioItemRepository extends JpaRepository<PortfolioItem, Integer> {
 }
