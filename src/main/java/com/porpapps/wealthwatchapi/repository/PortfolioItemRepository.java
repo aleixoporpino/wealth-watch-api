150b218320789e8b1ddac5a2cf2266bcadfbe2e9
Lucianna Mendonca <luciannamend@gmail.com>
2023-08-22 14:22:06 -0400

    GH-9 - Changed interface name

--- src/main/java/com/porpapps/wealthwatchapi/repository/PortfolioItemRepository.java
+++ src/main/java/com/porpapps/wealthwatchapi/repository/PortfolioItemRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Portfolio extends JpaRepository<Portfolio, Integer> {
+public interface PortfolioItemRepository extends JpaRepository<PortfolioItemRepository, Integer> {
 }
--- src/main/java/com/porpapps/wealthwatchapi/repository/PortfolioRepository.java
+++ src/main/java/com/porpapps/wealthwatchapi/repository/PortfolioRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PortfolioItem extends JpaRepository<PortfolioItem, Integer> {
+public interface PortfolioRepository extends JpaRepository<PortfolioRepository, Integer> {
 }
